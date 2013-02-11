ded33a61b39662aa9bdb74a254465a5110d96513
Stefan Arentz <stefan@arentz.ca>
2013-02-11 17:24:11 -0500

    iniial import

---
+++ bug-840208-camera-leaks-gps-info/upload.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import time
+
+from flask import Flask, request, jsonify
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/tmp'
+EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    print "FILES", request.files
+    file = request.files['file']
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({"success":True})
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.run("0.0.0.0", 8080)
